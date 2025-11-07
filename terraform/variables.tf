ccef9223ff82e0adbe3b5f80c7930ef3697bc56a
Pavankalyan29 <pavankalyan.pentam29@gmail.com>
2025-11-07 20:42:17 +0530

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.medium"
+  default     = "t3.medium"
 }
 
 variable "key_name" {
