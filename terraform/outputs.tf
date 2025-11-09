1c11cec05c73f168ed17e34956f17e8b5df1664d
Pavankalyan29 <pavankalyan.pentam29@gmail.com>
2025-11-09 21:40:01 +0530

    Updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,6 @@ output "ssh_command" {
   value = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.elk_instance.public_ip}"
 }
 
+output "kibana_url" {
+  value = "http://${aws_instance.elk_instance.public_ip}:5601"
+}
